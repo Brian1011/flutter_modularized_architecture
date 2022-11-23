5c224ef508458de560ac0cabdce244431e37fef4
Brian Mutinda <mutindabrian26@gmail.com>
2022-11-23 15:12:01 +0300

    create login and home module

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -1,14 +1,17 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:modularize/main.dart';
+import 'package:modularize/modules/home/home_module.dart';
+import 'package:modularize/modules/login/login_module.dart';
 
 class AppModule extends Module {
   @override
   List<ModularRoute> get routes => [
-        ChildRoute(Modular.initialRoute,
+        /*ChildRoute(Modular.initialRoute,
             child: (context, args) => const Home1()),
         ChildRoute('/home2/:name',
             child: (context, args) => Home2(
                   name: args.queryParams['name'],
-                ))
+                )),*/
+        ModuleRoute(Modular.initialRoute, module: LoginModule()),
+        ModuleRoute("/home", module: HomeModule())
       ];
 }
---
+++ lib/modules/home/home_module.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class HomeModule extends Module {
+  @override
+  List<Bind> get binds => const [];
+
+  @override
+  List<ModularRoute> get routes => const [];
+}
